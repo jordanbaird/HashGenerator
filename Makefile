5ae0cb1af502cf23dec9d5e4d413b608faa63220
Jordan Baird <jordanbaird.dev@gmail.com>
2023-03-08 07:11:34 -0700

    Remove docc-plugin

--- Makefile
+++
@@ -1,7 +0,0 @@
-generate_docs:
-	swift package --allow-writing-to-directory docs \
-		generate-documentation --target HashGenerator \
-		--disable-indexing \
-		--transform-for-static-hosting \
-		--hosting-base-path HashGenerator \
-		--output-path docs
--- Package.swift
+++ Package.swift
@@ -10,12 +10,6 @@ let package = Package(
             targets: ["HashGenerator"]
         ),
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/apple/swift-docc-plugin",
-            from: "1.0.0"
-        ),
-    ],
     targets: [
         .target(
             name: "HashGenerator",
